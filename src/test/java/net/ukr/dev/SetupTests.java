d3896e8d93264fff8c95ed728ce0b066b82864e1
s.kuliesh <s.kuliesh>
2018-04-11 17:39:00 +0300

    update file

--- src/test/java/net/ukr/dev/SetupTests.java
+++ src/test/java/net/ukr/dev/SetupTests.java
@@ -1,40 +1,34 @@
 package net.ukr.dev;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
-import java.util.concurrent.TimeUnit;
-
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class SetupTests  {
 
-    public static WebDriver driver;
-
     @BeforeClass
     public static void setup() {
+        System.setProperty("selenide.browser", "chrome");
         System.setProperty("webdriver.chrome.driver", "D:/AutomationTest/authtest/chromedriver.exe");
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("http://accounts.dev.ukr.net");
+        open("http://accounts.dev.ukr.net");
     }
 
     @Test
     public void openPage() {
-        driver.get("http://accounts.dev.ukr.net/registration");
-        assertTrue("Page open successes", $(".header__title").getText().equals("Регистрация почтового ящика"));
+        open("http://accounts.dev.ukr.net/registration");
+        assertEquals("Реєстрація поштової скриньки", $(".header__title").getText());
     }
 
     @AfterClass
     public static void tearDown(){
-        driver.close();
-        driver.quit();
+       // driver.close();
+        //driver.quit();
     }
 
 }
