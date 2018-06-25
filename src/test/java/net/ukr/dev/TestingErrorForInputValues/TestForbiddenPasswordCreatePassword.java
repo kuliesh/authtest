1106d4f05b493465d635defb8e933122cdec9b70
s.kuliesh <s.kuliesh>
2018-06-25 16:17:14 +0300

    add new test

---
+++ src/test/java/net/ukr/dev/TestingErrorForInputValues/TestForbiddenPasswordCreatePassword.java
@@ -0,0 +1,54 @@
+package net.ukr.dev.TestingErrorForInputValues;
+
+import net.ukr.dev.BasesClassesForTests.BaseTests;
+import net.ukr.dev.helpers.ApachePOIreadHelper;
+import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.Assert.assertFalse;
+
+public class TestForbiddenPasswordCreatePassword extends BaseTests {
+    final static Logger logger = Logger.getLogger(net.ukr.dev.TestingErrorForInputValues.TestForbiddenPasswordCreatePassword.class);
+
+    //Активація дата провайдера
+    @DataProvider
+    public Object[][] errorDataForPasswordUA (Method method){
+
+        ApachePOIreadHelper excelReader = new ApachePOIreadHelper();
+        File file = new File("D:/AutomationTest/authtest/data_file/forbidden_password.xlsx");
+        excelReader.setExcelFile(String.valueOf(file), "ForbiddenTest");
+        List rowsNo = excelReader.getRowContains(method.getName(), 0 );
+        return excelReader.getTableArray(rowsNo);
+    }
+
+    @Test(dataProvider = "errorDataForPasswordUA")
+    //формування масиву даних з файла, що був оброблений дата провайдером
+    public void forbiddenPassword(ArrayList data) throws InterruptedException {
+        String testName = String.valueOf(data.get(0));
+        String errorPassword = String.valueOf(data.get(1));
+        String errorText = String.valueOf(data.get(2));
+        logger.info("errorPassword = " + errorPassword);
+        logger.info("errorText = " + errorText);
+        logger.info("testName = " + testName);
+
+        open("http://accounts-new.dev.ukr.net/registration");
+        Thread.sleep(2000);
+
+        //Вже сам тест
+        $(By.cssSelector("#id-password")).sendKeys(errorPassword);
+        $(By.cssSelector("#id-password-repeat")).click();
+        $(By.cssSelector(".input-default__error.is-size-normal")).shouldHave(text(errorText));
+        assertFalse("No user found.",$(By.cssSelector(".input-default__error.is-size-normal")).getText().equals(errorText));
+        Thread.sleep(2000);
+        refresh();
+    }
+}
