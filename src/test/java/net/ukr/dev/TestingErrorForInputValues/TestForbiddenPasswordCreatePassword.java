9a5f1240e293eb0a7f55c12daf2996660c1031e0
s.kuliesh <s.kuliesh>
2018-06-25 16:54:50 +0300

    update

--- src/test/java/net/ukr/dev/TestingErrorForInputValues/TestForbiddenPasswordCreatePassword.java
+++ src/test/java/net/ukr/dev/TestingErrorForInputValues/TestForbiddenPasswordCreatePassword.java
@@ -16,12 +16,13 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.Assert.assertFalse;
 
+@Test
 public class TestForbiddenPasswordCreatePassword extends BaseTests {
     final static Logger logger = Logger.getLogger(net.ukr.dev.TestingErrorForInputValues.TestForbiddenPasswordCreatePassword.class);
 
     //Активація дата провайдера
     @DataProvider
-    public Object[][] errorDataForPasswordUA (Method method){
+    public Object[][] errorDataForPasswordUa (Method method){
 
         ApachePOIreadHelper excelReader = new ApachePOIreadHelper();
         File file = new File("D:/AutomationTest/authtest/data_file/forbidden_password.xlsx");
@@ -30,7 +31,7 @@ public class TestForbiddenPasswordCreatePassword extends BaseTests {
         return excelReader.getTableArray(rowsNo);
     }
 
-    @Test(dataProvider = "errorDataForPasswordUA")
+    @Test(dataProvider = "errorDataForPasswordUa")
     //формування масиву даних з файла, що був оброблений дата провайдером
     public void forbiddenPassword(ArrayList data) throws InterruptedException {
         String testName = String.valueOf(data.get(0));
@@ -40,7 +41,7 @@ public class TestForbiddenPasswordCreatePassword extends BaseTests {
         logger.info("inputPassword = " + inputPassword);
         logger.info("displayedError = " + displayedError);
 
-        open("http://accounts-new.dev.ukr.net/registration");
+        open("https://accounts.ukr.net/registration");
         Thread.sleep(2000);
 
         //Вже сам тест
--- src/test/java/net/ukr/dev/TestingWidjet/TestDisplayedElementsForWidgetUkr.java
+++ src/test/java/net/ukr/dev/TestingWidjet/TestDisplayedElementsForWidgetUkr.java
@@ -3,12 +3,12 @@ package net.ukr.dev.TestingWidjet;
 import net.ukr.dev.BasesClassesForTests.BaseTests;
 import net.ukr.dev.TimeLimiteTest;
 import org.apache.log4j.Logger;
+import org.openqa.selenium.By;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.testng.Assert.assertTrue;
 import static org.testng.AssertJUnit.assertEquals;
 
 public class TestDisplayedElementsForWidgetUkr extends BaseTests {
@@ -17,67 +17,12 @@ public class TestDisplayedElementsForWidgetUkr extends BaseTests {
     @BeforeClass
     public void openPage() throws InterruptedException {
         logger.info("Open page for testing widget");
-        open("https://www.ukr.net/"); //відкриваємо сторінку входу до поштової скриньки
+        open("http://morty.fwdcdn.com/widget/widget.html"); //відкриваємо сторінку входу до поштової скриньки
     }
 
     @Test
     public void Wg1_Title (){
-        assertEquals("Пошта", $((".login-block__title")).getText());
+        assertEquals("Пошта", $(By.xpath("//form/div[1]/a/text()")).getText());
         logger.info("Заголовок 'Пошта' відображається");
-    }
-
-    @Test
-    public void Wg1_TitleClick (){
-
-    }
-
-    @Test
-    public void Wg2_FieldLogin (){
-        assertTrue($(("#id-input-login")).isDisplayed());
-        logger.info("Поле 'Ім’я вашої скриньки' відображається");
-    }
-
-    @Test
-    public void Wg3_FieldPassword (){
-        assertTrue($(("#id-input-password")).isDisplayed());
-        logger.info("Поле 'Пароль' відображається");
-    }
-
-    @Test
-    public void Wg4_CheckBoxOthComp (){
-        assertTrue($((".form__short-session-label")).isDisplayed());
-        logger.info("Чекбокс 'Чужий комп'ютер' відображається");
-        assertEquals("Чужий комп'ютер", $(".form__short-session-label").getText());
-        logger.info("Строка 'Чужий комп'ютер' відображається");
-    }
-
-    @Test
-    public void Wg5_ButtonEnter (){
-        assertEquals("Create Your @UKR.NET Mailbox", $(".header__title").getText());
-        logger.info("Строка 'Create Your @UKR.NET Mailbox' відображається");
-    }
-
-    @Test
-    public void Wg6_ButtonNotEnter (){
-        assertEquals("Create Your @UKR.NET Mailbox", $(".header__title").getText());
-        logger.info("Строка 'Create Your @UKR.NET Mailbox' відображається");
-    }
-
-    @Test
-    public void Wg6_ButtonNotEnterClick (){
-        assertEquals("Create Your @UKR.NET Mailbox", $(".header__title").getText());
-        logger.info("Строка 'Create Your @UKR.NET Mailbox' відображається");
-    }
-
-    @Test
-    public void Wg7_ButtonCreate (){
-        assertEquals("Create Your @UKR.NET Mailbox", $(".header__title").getText());
-        logger.info("Строка 'Create Your @UKR.NET Mailbox' відображається");
-    }
-
-    @Test
-    public void Wg7_ButtonCreateClick (){
-        assertEquals("Create Your @UKR.NET Mailbox", $(".header__title").getText());
-        logger.info("Строка 'Create Your @UKR.NET Mailbox' відображається");
     }
 }
