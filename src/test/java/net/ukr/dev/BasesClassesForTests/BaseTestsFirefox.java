51b009269d5cfd33b56f9a9649adff55a35426c8
s.kuliesh <s.kuliesh>
2018-06-26 19:51:03 +0300

    update

--- src/test/java/net/ukr/dev/BasesClassesForTests/BaseTestsFirefox.java
+++ src/test/java/net/ukr/dev/BasesClassesForTests/BaseTestsFirefox.java
@@ -10,8 +10,10 @@ public class BaseTestsFirefox {
     @BeforeTest
     public static void setup() {
         logger.info("Run browser...");
-        System.setProperty("selenide.browser", "firefox");
-        System.setProperty("webdriver.chrome.driver", "D:/AutomationTest/authtest/geckodriver.exe");
+//        System.setProperty("selenide.browser", "firefox");
+//        System.setProperty("webdriver.chrome.driver", "D:/AutomationTest/authtest/geckodriver.exe");
+        System.setProperty("selenide.browser", "opera");
+        System.setProperty("webdriver.opera.driver", "D:/AutomationTest/authtest/operadriver.exe");
     }
 
     @AfterTest
